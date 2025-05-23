260789e07a002b4c382388f13c8183c1744e2d6c
yuricifiello <yuricifiello@gmail.com>
2025-05-23 16:25:16 +0200

    add title

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -1,3 +1,10 @@
 export default function Header() {
-  return <header></header>;
+  return (
+    <header>
+      <div>
+        {" "}
+        <h1> Learn Web development </h1>
+      </div>
+    </header>
+  );
 }
