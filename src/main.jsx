708d8457781f53b68c816838c9603fb713959891
yuricifiello <yuricifiello@gmail.com>
2025-05-23 16:17:04 +0200

    add bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+
+// IMPORT BOOTSTRAP
+import "bootstrap/dist/css/bootstrap.min.css";
+import * as bootstrap from "bootstrap";
+
 import "./assets/css/index.css";
 import App from "./App.jsx";
 
